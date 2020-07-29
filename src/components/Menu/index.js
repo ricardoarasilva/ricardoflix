378472f6e437a9af6425355fccd93f5de66ebbe7
Ricardo Silva <ricardo.silva@complemento.net.br>
2020-07-29 16:43:42 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 import Button from '../Button'
@@ -10,7 +11,7 @@ function Menu() {
                 <img className="Logo" src={Logo} alt="Alura Flix Logo" />
             </a>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
